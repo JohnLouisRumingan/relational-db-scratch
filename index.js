0a7f809d82812e3746cc4151c88e98691ee800db
John Louis Rumingan <jmrumingan@gmail.com>
2020-08-18 10:43:47 -0400

    Add record constructor

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+// create factory function or class? We can use class here to make it more OOP-like
+// We will create a simple relationship between employees of an organization.
+
+class Record{
+    constructor(name, position, building){
+        this.name = name;
+        this.position = position;
+        this.building = building;
+    }
+
+    
+}
